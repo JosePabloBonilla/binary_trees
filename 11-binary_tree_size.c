ceccab65b227b77c17e940625378dc2586de3c13
yatoro667 <ytorres667@gmail.com>
2021-07-22 11:52:54 +0000

    Adding 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures size of binary tree
+ * @tree: root of tree
+ * Return: size
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t c = 1;
+
+	if (!tree)
+		return (0);
+	
+	c += binary_tree_size(tree->left);
+	c += binary_tree_size(tree->right);
+
+	return (c);
+}
